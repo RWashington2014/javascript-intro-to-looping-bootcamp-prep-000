511189170446beecdcbebc17020fc4f49c6cf6e4
Robert W <robwash@gmail.com>
2017-09-09 14:36:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,8 +5,13 @@ function forLoop(array){
     var array = []
 
   for(let i = 0; i < 25; i++){
+    if(i === 1){
+      array = [`"I am 1 strange loop"`]
+    }
+    else{
       array = [`"I am ${i} strange loops"`, ...array]
-  }
+    }  
+}
   return array
 }
 
