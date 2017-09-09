38557b55ad0e570bbc5f432279ea4cb0211ed761
Robert W <robwash@gmail.com>
2017-09-09 15:11:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,7 +8,10 @@ function forLoop(array){
   for(let i = 0; i < 25; i++){
 
       if (i === 1) {
-      array = [`"I am ${i} strange loops."`, ...array]
+        array = [`"I am ${i} strange loops."`, ...array]
+      }
+      else {
+        array = [`"I am ${i} strange loops."`, ...array]
       }
 }
   return array
